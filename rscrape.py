0f51a6b88fee4250185ac36c0d1baeb5ee029c91
kynes446 <mattlee446@gmail.com>
2015-08-21 11:58:21 +0900

    made method to get a request for a certain class category

--- rscrape.py
+++ rscrape.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from requests import request
 from time import sleep
 
+
+
 headies = {
 	'Accept':'*/*',
 	'Accept-Encoding':'gzip, deflate',
@@ -12,15 +14,14 @@ headies = {
 	'Connection':'keep-alive',
 	'Content-Length':'14069',
 	'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-	'Cookie':'__utma=29573820.1895064751.1440056326.1440056326.1440056326.1; __utmc=29573820; __utmz=29573820.1440056326.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%%20provided); _ga=GA1.2.1895064751.1440056326',
 	'Host':'cdcs.ur.rochester.edu',
 	'Origin':'https://cdcs.ur.rochester.edu',
 	'Referer':'https://cdcs.ur.rochester.edu/',
 	'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
 	'X-MicrosoftAjax':'Delta=true',
 }
-s = requests.Session()
-data = s.get(url='https://cdcs.ur.rochester.edu/Default.aspx')
+
+data = request(method='GET', url='https://cdcs.ur.rochester.edu/Default.aspx')
 
 soup = BeautifulSoup(data.content)
 # parse and retrieve three vital form values
@@ -30,8 +31,10 @@ eventvalidation = soup.select("#__EVENTVALIDATION")[0]['value']
 
 
 
-print viewstategen
-formData = {
+
+def getpage(category):
+	global headies
+	form_data = {
 	'ScriptManager1':'UpdatePanel4|btnSearchTop',
 	'__LASTFOCUS':'',
 	'__EVENTTARGET':'',
@@ -41,7 +44,7 @@ formData = {
 	'__EVENTVALIDATION': eventvalidation,
 	'ddlTerm':'D-20161',
 	'ddlSchool':'',
-	'ddlDept':'AAS',
+	'ddlDept':category,
 	'txtCourse':'',
 	'ddlTypes':'',
 	'ddlStatus':'',
@@ -55,9 +58,11 @@ formData = {
 	'ddlDivision':'',
 	'__ASYNCPOST':'true',
 	'btnSearchTop':'Search',
-}
+	}
+	classinfo = request(method='POST', url='https://cdcs.ur.rochester.edu/Default.aspx', headers=headies, data=form_data)
+	print classinfo.content
+	return classinfo
 
-data2 = s.post(url='https://cdcs.ur.rochester.edu/Default.aspx', headers=headies, data=formData)
-sleep(5)
-print data2.content
-print data.headers
+
+
+getpage('AAS')
